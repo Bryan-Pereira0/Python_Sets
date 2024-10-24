f594eeb984c015924490fde9f89df83c359881b6
haru <gfxmagicz@gmail.com>
2024-10-24 13:41:43 -0400

    Update Set_Operations_In_Data_Analysis.py
    My git was not picking up the changes had to copy paste the code from my IDE to here

--- Set_Operations_In_Data_Analysis.py
+++ Set_Operations_In_Data_Analysis.py
@@ -1,4 +1,4 @@
 #task1
 customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
 unique_customer = set(customer_ids)
-print ("Unique customer ID: ",unique_customer)
+print ("Unique customer ID: ",sorted(unique_customer))
